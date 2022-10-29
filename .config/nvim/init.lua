4c75ceef317293834f59ddb0e26b9ce6eeed4b7a
Jack Yu <jackyu1996@hotmail.com>
2022-10-29 20:41:51 +0800

    chore: update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,43 +1,70 @@
+local api = vim.api
 local fn = vim.fn
 local g = vim.g
-local map = vim.api.nvim_set_keymap
+local lsp = vim.lsp
+local mapkey = vim.keymap.set
 local opt = vim.opt
--- local autocmd = vim.api.nvim_create_autocmd
--- local cmd = vim.api.nvim_command
 
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({
         "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path
-    }
-    )
-    vim.cmd [[packadd packer.nvim]]
+    })
+    api.cmd [[packadd packer.nvim]]
 end
 
 g.mapleader = " "
 g.maplocalleader = " "
 
+g.gui_font_default_size = 12
+g.gui_font_size = g.gui_font_default_size
+g.gui_font_face = "FiraCode Nerd Font"
+
+RefreshGuiFont = function()
+    opt.guifont = string.format("%s:h%s", g.gui_font_face, g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+    g.gui_font_size = g.gui_font_size + delta
+    RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+    g.gui_font_size = g.gui_font_default_size
+    RefreshGuiFont()
+end
+
 local mapping_opts = { noremap = true, silent = true }
 
-map("i", "jk", "<ESC>", mapping_opts)
-map("c", "<C-n>", "<down>", mapping_opts)
-map("c", "<C-p>", "<up>", mapping_opts)
-map("n", "<C-h>", "<C-w>h", mapping_opts)
-map("n", "<C-j>", "<C-w>j", mapping_opts)
-map("n", "<C-k>", "<C-w>k", mapping_opts)
-map("n", "<C-l>", "<C-w>l", mapping_opts)
-map("n", "<F11>", ":terminal<CR>", mapping_opts)
-map("n", "<leader>b", ":SymbolsOutline<CR>", mapping_opts)
-map("n", "<leader>e", ":TroubleToggle<CR>", mapping_opts)
-map("n", "<leader>h", ":set hlsearch!<CR>", mapping_opts)
-map("n", "<leader>q", ":quit!<CR>", mapping_opts)
-map("n", "<leader>w", ":write<CR>", mapping_opts)
-map("n", "<leader>x", ":quitall!<CR>", mapping_opts)
-map("n", "<F8>", ":NvimTreeToggle<CR>", mapping_opts)
-map("n", "<leader>p", ":Telescope find_files<CR>", mapping_opts)
-map("n", "<leader>r", ":Telescope live_grep<CR>", mapping_opts)
-map("n", "<leader>n", ":Telescope buffers<CR>", mapping_opts)
-map("n", "<leader>k", ":Telescope keymaps<CR>", mapping_opts)
+mapkey("c", "<C-n>", "<down>", mapping_opts)
+mapkey("c", "<C-p>", "<up>", mapping_opts)
+mapkey("i", "jk", "<ESC>", mapping_opts)
+mapkey("n", "<C-+>", "<cmd>lua ResizeGuiFont(1)<CR>", mapping_opts)
+mapkey("n", "<C-->", "<cmd>lua ResizeGuiFont(-1)<CR>", mapping_opts)
+mapkey("n", "<C-BS>", ResetGuiFont, mapping_opts)
+mapkey("n", "<C-h>", "<C-w>h", mapping_opts)
+mapkey("n", "<C-j>", "<C-w>j", mapping_opts)
+mapkey("n", "<C-k>", "<C-w>k", mapping_opts)
+mapkey("n", "<C-l>", "<C-w>l", mapping_opts)
+mapkey("n", "<F11>", "<cmd>terminal<CR>", mapping_opts)
+mapkey("n", "<F5>", "<cmd>DapContinue<CR>", mapping_opts)
+mapkey("n", "<F6>", "<cmd>DapToggleBreakpoint<CR>", mapping_opts)
+mapkey("n", "<F7>", "<cmd>DapStepOver<CR>", mapping_opts)
+mapkey("n", "<F8>", "<cmd>DapStepInto<CR>", mapping_opts)
+mapkey("n", "<F9>", "<cmd>DapStepOut<CR>", mapping_opts)
+mapkey("n", "<leader>b", "<cmd>SymbolsOutline<CR>", mapping_opts)
+mapkey("n", "<leader>d", "<cmd>lua require('dapui').toggle()<CR>", mapping_opts)
+mapkey("n", "<leader>e", "<cmd>TroubleToggle<CR>", mapping_opts)
+mapkey("n", "<leader>h", "<cmd>set hlsearch!<CR>", mapping_opts)
+mapkey("n", "<leader>k", "<cmd>Telescope keymaps<CR>", mapping_opts)
+mapkey("n", "<leader>n", "<cmd>Telescope buffers<CR>", mapping_opts)
+mapkey("n", "<leader>p", "<cmd>Telescope find_files<CR>", mapping_opts)
+mapkey("n", "<leader>q", "<cmd>quit!<CR>", mapping_opts)
+mapkey("n", "<leader>r", "<cmd>Telescope live_grep<CR>", mapping_opts)
+mapkey("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", mapping_opts)
+mapkey("n", "<leader>w", "<cmd>write<CR>", mapping_opts)
+mapkey("n", "<leader>x", "<cmd>quitall!<CR>", mapping_opts)
+mapkey("x", "<leader>a", "<cmd>EasyAlign<CR>", mapping_opts)
 
 opt.autoindent = true
 opt.autoread = true
@@ -78,8 +105,24 @@ opt.undofile = true
 opt.virtualedit = "block"
 opt.whichwrap = "b,s,h,l,<,>,[,]"
 
+api.nvim_create_augroup("LspAttach_inlayhints", {})
+api.nvim_create_autocmd("LspAttach", {
+    group = "LspAttach_inlayhints",
+    callback = function(args)
+        if not (args.data and args.data.client_id) then
+            return
+        end
+
+        local bufnr = args.buf
+        local client = lsp.get_client_by_id(args.data.client_id)
+        require("lsp-inlayhints").on_attach(client, bufnr)
+    end,
+})
+
 require("packer").startup(function(use)
     use { "wbthomason/packer.nvim" }
+
+    use { "junegunn/vim-easy-align" }
 
     use {
         "nvim-treesitter/nvim-treesitter",
@@ -100,9 +143,7 @@ require("packer").startup(function(use)
         end
     }
 
-    use {
-        "neovim/nvim-lspconfig",
-    }
+    use { "neovim/nvim-lspconfig" }
 
     use {
         "williamboman/mason.nvim",
@@ -131,22 +172,26 @@ require("packer").startup(function(use)
         "hrsh7th/cmp-nvim-lsp",
         config = function()
             local on_attach = function(client, bufnr)
-                vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+                -- warning: alias variables for vim have to be redefined
+                local mapkey = vim.keymap.set
+                local lsp = vim.lsp
+                local api = vim.api
+
+                api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
                 local bufopts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-                vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-                vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+                mapkey("n", "gD", lsp.buf.declaration, bufopts)
+                mapkey("n", "gd", lsp.buf.definition, bufopts)
+                mapkey("n", "gr", lsp.buf.references, bufopts)
+                mapkey("n", "gi", lsp.buf.implementation, bufopts)
+                mapkey("n", "K", lsp.buf.hover, bufopts)
+                mapkey("n", "<C-k>", lsp.buf.signature_help, bufopts)
+                mapkey("n", "<space>D", lsp.buf.type_definition, bufopts)
+                mapkey("n", "<space>rn", lsp.buf.rename, bufopts)
+                mapkey("n", "<space>ca", lsp.buf.code_action, bufopts)
+                mapkey("n", "<space>f", lsp.buf.format, bufopts)
             end
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local servers =
             { "bashls", "clangd", "cmake", "cssls", "dartls", "denols", "emmet_ls", "eslint", "gopls", "html", "jdtls",
@@ -162,6 +207,13 @@ require("packer").startup(function(use)
     }
 
     use {
+        "lvimuser/lsp-inlayhints.nvim",
+        config = function()
+            require("lsp-inlayhints").setup()
+        end
+    }
+
+    use {
         "honza/vim-snippets",
         event = "InsertEnter",
     }
@@ -173,27 +225,29 @@ require("packer").startup(function(use)
         end
     }
 
-    use {
-        "saadparwaiz1/cmp_luasnip"
-    }
+    use { "saadparwaiz1/cmp_luasnip" }
 
-    use "hrsh7th/cmp-buffer"
+    use { "hrsh7th/cmp-buffer" }
 
-    use "hrsh7th/cmp-path"
+    use { "hrsh7th/cmp-path" }
 
-    use "hrsh7th/cmp-cmdline"
+    use { "hrsh7th/cmp-cmdline" }
+
+    use { "dmitmel/cmp-digraphs" }
 
     use {
         "hrsh7th/nvim-cmp",
         config = function()
-            local has_words_before = function()
-                local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
+            local api = vim.api
 
+            local has_words_before = function()
+                local line, col = table.unpack(api.nvim_win_get_cursor(0))
+                return col ~= 0 and
+                    api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
             local luasnip = require("luasnip")
             local cmp = require("cmp")
+
             cmp.setup {
                 snippet = {
                     expand = function(args)
@@ -210,10 +264,31 @@ require("packer").startup(function(use)
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
+                    { name = "digraphs" },
                 }, {
                     { name = "buffer" },
                 })
@@ -240,7 +315,9 @@ require("packer").startup(function(use)
     use {
         "windwp/nvim-autopairs",
         config = function()
-            require("nvim-autopairs").setup {}
+            require("nvim-autopairs").setup {
+                fast_wrap = {}
+            }
         end
     }
 
@@ -253,9 +330,7 @@ require("packer").startup(function(use)
 
     use {
         "kyazdani42/nvim-tree.lua",
-        requires = {
-            "kyazdani42/nvim-web-devicons",
-        },
+        requires = { "kyazdani42/nvim-web-devicons" },
         config = function()
             require("nvim-tree").setup {}
         end
@@ -263,9 +338,7 @@ require("packer").startup(function(use)
 
     use {
         "nvim-telescope/telescope.nvim", tag = "0.1.0",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
+        requires = { "nvim-lua/plenary.nvim" },
         config = function()
             require("telescope").setup {
                 defaults = {
@@ -284,7 +357,88 @@ require("packer").startup(function(use)
     }
 
     use {
-        "mfussenegger/nvim-dap"
+        "mfussenegger/nvim-dap",
+        config = function()
+            local fn = vim.fn
+            local dap = require('dap')
+
+            dap.adapters.lldb = {
+                type = "executable",
+                command = "/usr/bin/lldb-vscode",
+                name = "lldb"
+            }
+            dap.configurations.c = {
+                {
+                    name = 'Launch',
+                    type = 'lldb',
+                    request = 'launch',
+                    program = function()
+                        return fn.input('Path to executable: ', fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+            dap.configurations.rust = dap.configurations.c
+
+            dap.adapters.delve = {
+                type = 'server',
+                port = '${port}',
+                executable = {
+                    command = 'dlv',
+                    args = { 'dap', '-l', '127.0.0.1:${port}' },
+                }
+            }
+            dap.configurations.go = {
+                {
+                    type = "delve",
+                    name = "Debug",
+                    request = "launch",
+                    program = "${file}"
+                },
+                {
+                    type = "delve",
+                    name = "Debug test (go.mod)",
+                    request = "launch",
+                    mode = "test",
+                    program = "./${relativeFileDirname}"
+                }
+            }
+
+            local venv = os.getenv("VIRTUAL_ENV")
+            local executable = fn.getcwd() .. string.format("%s/bin/python", venv)
+            dap.adapters.python = {
+                type = 'executable',
+                command = (venv == nil and "/usr/bin/python" or executable),
+                args = { '-m', 'debugpy.adapter' }
+            }
+            dap.configurations.python = {
+                {
+                    type = 'python';
+                    request = 'launch';
+                    name = "Launch file";
+                    program = "${file}";
+                    pythonPath = function()
+                        local cwd = fn.getcwd()
+                        if fn.executable(cwd .. '/venv/bin/python') == 1 then
+                            return cwd .. '/venv/bin/python'
+                        elseif fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                            return cwd .. '/.venv/bin/python'
+                        else
+                            return '/usr/bin/python'
+                        end
+                    end;
+                },
+            }
+        end
+    }
+
+    use {
+        "rcarriga/nvim-dap-ui",
+        config = function()
+            require("dapui").setup {}
+        end
     }
 
     use {
@@ -366,13 +520,6 @@ require("packer").startup(function(use)
         "lewis6991/spellsitter.nvim",
         config = function()
             require("spellsitter").setup()
-        end
-    }
-
-    use {
-        "lewis6991/impatient.nvim",
-        config = function()
-            require("impatient")
         end
     }
 end)
